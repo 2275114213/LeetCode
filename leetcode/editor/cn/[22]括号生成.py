12b079588aedd7d30a1df54826fe68501e07d68f
liuxiaolei <2275114213@qq.com>
2020-11-01 22:20:11 +0800

    at home

---
+++ leetcode/editor/cn/[22]括号生成.py
@@ -0,0 +1,27 @@
+# 数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。 
+# 
+#  
+# 
+#  示例： 
+# 
+#  输入：n = 3
+# 输出：[
+#        "((()))",
+#        "(()())",
+#        "(())()",
+#        "()(())",
+#        "()()()"
+#      ]
+#  
+#  Related Topics 字符串 回溯算法 
+#  👍 1393 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+# leetcode submit region end(Prohibit modification and deletion)
